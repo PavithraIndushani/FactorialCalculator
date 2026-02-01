4aa3395746871e0eb95cb063a7825b5e4b4de188
PavithraIndushani <200076700716bci@gmail.com>
2026-02-01 11:10:56 +0530

    Initial commit

---
+++ FactorialCalculator1.java
@@ -0,0 +1,17 @@
+public class FactorialCalculator1 {
+public static int calculateFactorial(int number) {
+if (number < 0) {
+throw new IllegalArgumentException("Factorial is not defined fornegative numbers");
+}
+int factorialResult = 1;
+for (int i = 1; i <= number; i++) {
+factorialResult *= i;
+}
+return factorialResult;
+}
+public static void main(String[] args) {
+int number = 5;
+int result = calculateFactorial(number);
+System.out.println("Factorial of " + number + " is: " + result);
+}
+} 
